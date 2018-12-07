9ed72c3f77d87271db410879a26d203755e5d2aa
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2018-12-07 15:43:54 +0200

    Use onScrolled instead

--- frontend/android/app/src/main/java/xyz/shmeleva/eight/fragments/ChatFragment.kt
+++ frontend/android/app/src/main/java/xyz/shmeleva/eight/fragments/ChatFragment.kt
@@ -149,12 +149,10 @@ class ChatFragment : Fragment() {
         chatMessagesRecyclerView.layoutManager = layoutManager
         chatMessagesRecyclerView.adapter = adapter
         chatMessagesRecyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
                 val lastVisiblePosition = layoutManager.findLastCompletelyVisibleItemPosition()
                 val lastPosition = adapter.itemCount - 1
-
                 shouldScrollToBottom = lastVisiblePosition == lastPosition
             }
         })
