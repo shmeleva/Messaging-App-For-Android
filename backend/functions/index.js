4c9d9fd0a7868e2332769d7cad41d83b58ac09b8
Satenik Hovsepyan <satenik.hovsepyan@aalto.fi>
2018-12-08 19:22:59 +0200

    cloud functions fixes

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,14 +3,12 @@ const functions = require('firebase-functions');
 
 admin.initializeApp();
 
-const imageResizing = require('./resizeImage')
-const imageLabeling = require('./labelImage')
-const newChatNotification = require('./notifyNewChat');
-const newMessageNotification = require('./notifyNewMessage');
-const addedToChatNotification = require('./notifyAddedToChat');
+const resizeImage = require('./resizeImage')
+const labelImage = require('./labelImage')
+const notifyNewMessage = require('./notifyNewMessage');
+const notifyNewGroupChat = require('./notifyNewGroupChat');
 
-exports.imageLabelingMessage = imageLabeling.imageLabelingMessage;
-exports.imageResizing = imageResizing.changeImageSize;
-exports.newChatNotification = newChatNotification.handler;
-exports.newMessageNotification = newMessageNotification.handler;
-exports.addedToChatNotification = addedToChatNotification.handler;
+exports.labelImage = labelImage.imageLabelingMessage;
+exports.resizeImage = resizeImage.changeImageSize;
+exports.notifyNewMessage = notifyNewMessage.handler;
+exports.notifyNewGroupChat = notifyNewGroupChat.handler;
